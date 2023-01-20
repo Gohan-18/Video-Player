90d706f038e2098f0100c612a569b71c0e59289b
Prabhat <112978778+Gohan-18@users.noreply.github.com>
2023-01-20 17:41:26 +0530

    firebase initiated

---
+++ Video-Player/src/firebase/Auth.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOEGGnZEyUmQOK6MCTmKS3RqoUYTcZpVs",
+  authDomain: "clone-c029e.firebaseapp.com",
+  projectId: "clone-c029e",
+  storageBucket: "clone-c029e.appspot.com",
+  messagingSenderId: "758773996535",
+  appId: "1:758773996535:web:949429175327a6e9ab2cf2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
